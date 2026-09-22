1507fbb99811e309b3d93aa6555f5f0d0b661606
aitopmusicnews-cloud <aitopmusicnews@gmail.com>
2026-09-21 23:59:20 -0700

    feat: route promo workspace

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import { Editor } from "./routes/Editor.js";
+import { PromoWorkspace } from "./components/PromoWorkspace.js";
 
 const router = createBrowserRouter([
   { path: "/", element: <Editor /> },
+  { path: "/promo", element: <PromoWorkspace /> },
 ]);
 
 createRoot(document.getElementById("root")!).render(
